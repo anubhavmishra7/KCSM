479ae513526c7d4cba52b3b5a0cf893636c5766c
anubhavmishra7 <anubhavcv77@gmail..com>
2022-12-12 20:19:14 +0530

    12-12

--- lib/data/casestudy_list.dart
+++ lib/data/casestudy_list.dart
@@ -2,8 +2,8 @@ import 'package:kcsm/models/bookmarks.dart';
 
 final List<Caselist> caselists = [
   Caselist(1, "Saudi International Development\nFund",
-      "assets/images/aboutkaar2.png", "KSA"),
-  Caselist(2, "SAGIS", "assets/images/aboutkaar2.png", "UK"),
+      "assets/images/casestudy2.png", "KSA"),
+  Caselist(2, "SAGIS", "assets/images/casestudy1.png", "UK"),
   Caselist(
       3, "Almarai ECC Implementation", "assets/images/aboutkaar2.png", "USA"),
   Caselist(4, "Oman Methanol Company", "assets/images/landing1.png", "KSA"),
